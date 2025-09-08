e093dfb549a52788e2b4e41d9f06689b5728d301
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-08 09:52:11 +0900

    Add new feature or describe changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nihongo_playground/',
+  base: '/nihongo_playground/', // must match your repo name
 })
