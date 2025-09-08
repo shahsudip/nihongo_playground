ecdcf075e806c95a6b71e5edb1961a1cc8cad9d1
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-08 09:38:27 +0900

    Add new feature or describe changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/app_style.css'; // Global styles
 
 import LandingPage from './components/landing_screen.jsx';
 import LevelSelectionPage from './components/level_selection_screen.jsx';
-import QuizPage from './components/quiz_screen.jsx'; // Or your quiz_screen.jsx
+import QuizPage from './components/quiz_screen.jsx';
 import ResultsPage from './components/result_screen.jsx';
-import ProfilePage from './components/profile_screen.jsx'; // <-- Import the new page
+import ProfilePage from './components/profile_screen.jsx';
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/profile" element={<ProfilePage />} /> {/* <-- Add the new route */}
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/levels" element={<LevelSelectionPage />} />
         <Route path="/quiz/:level/:category" element={<QuizPage />} />
         <Route path="/results" element={<ResultsPage />} />
@@ -21,6 +21,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
