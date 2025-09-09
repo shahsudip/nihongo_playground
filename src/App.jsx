9b4f039b44103b6c0f1377bcb0650ed4a7bb152a
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-09 10:47:26 +0900

    custom quiz part

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,20 @@ import QuizPage from './components/quiz_screen.jsx';
 import ResultsPage from './components/result_screen.jsx';
 import ProfilePage from './components/profile_screen.jsx';
 
-
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-
-        <Route element={<MainLayout />}>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/levels" element={<LevelSelectionPage />} />
-          <Route path="/create" element={<CreateQuizPage />} />
-          {/* Route for standard N-level quizzes */}
-          <Route path="/quiz/:level/:category" element={<QuizPage />} />
-          {/* NEW Route for custom quizzes */}
-          <Route path="/quiz/custom/:quizId" element={<QuizPage />} />
-          <Route path="/results" element={<ResultsPage />} />
-        </Route>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/levels" element={<LevelSelectionPage />} />
+        <Route path="/quiz/:level/:category" element={<QuizPage />} />
+        <Route path="/results" element={<ResultsPage />} />
       </Routes>
     </Router>
   );
 }
+
 
 
 export default App;
