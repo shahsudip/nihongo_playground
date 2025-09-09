f155429100d4793503001c991819413f54b8460b
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-09 14:39:27 +0900

    custom quiz part

--- src/logic/use_quiz_manager.jsx
+++ src/logic/use_quiz_manager.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { quizData as staticQuizData } from '../data/quiz_data.js';
+import { quizData as staticQuizData } from '../data/quiz_data.jsx';
 
 export const useQuizManager = () => {
   const [allQuizzes, setAllQuizzes] = useState([]);
