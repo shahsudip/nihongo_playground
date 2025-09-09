86190704bdc99985d50a5cda70cf770ac02b86ef
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-09 14:36:57 +0900

    custom quiz part

--- src/components/profile_screen.jsx
+++ src/components/profile_screen.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import JapaneseText from '../components/JapaneseText.jsx';
-import { useQuizManager } from '../hooks/use_quiz_manager.js';
+import { useQuizManager } from '../logic/use_quiz_manager.js';
 
 const parseCsvToQuizContent = (csvText) => {
   if (!csvText) return [];
