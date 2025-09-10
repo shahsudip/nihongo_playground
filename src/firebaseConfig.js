556fddd57f4911e5d15c4548f5cfb390d2eb14d3
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-10 09:44:32 +0900

    custom quiz part

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // <-- THIS LINE WAS MISSING
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCWBeflLL18D730hf9lOPscL_GUQnbmLdg",
   authDomain: "study-planner-40bc8.firebaseapp.com",
@@ -21,9 +17,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 // Get references to the services
 const db = getFirestore(app);
-const auth = getAuth(app); // <-- 2. Initialize Auth
+const auth = getAuth(app);
 
-// 3. Export everything
+// Export the instances to be used in other components
 export { db, auth };
+
