7e305f4cdb992d50ab5246d4c6337f6b20214c6c
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-16 13:22:50 +0900

    fix: implement stealth scraping to bypass Cloudflare

--- scraper-test/index.js
+++ scraper-test/index.js
@@ -1,7 +1,7 @@
 // scraper/index.js
 process.env.LEVEL = 'n5';
 process.env.CATEGORY = 'kanji';
-process.env.EXERCISE = '1';
+process.env.EXERCISE = '01';
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 const puppeteer = require('puppeteer-extra');
