0928d9c21a8f9a3b7f00d7eb879289c8caa51541
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-20 15:42:48 +0900

    work on jlpt test UI

--- scraper-test/index.js
+++ scraper-test/index.js
@@ -18,7 +18,7 @@ console.log('JLPT Quiz Scraper started with Stealth Mode.');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-const LEVELS = ['n5', 'n4'];
+const LEVELS = ['n5'];
 
 const TEST_CATEGORIES = ['grammar', 'vocabulary', 'kanji', 'reading'];
 
