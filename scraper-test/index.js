3c7d9ee3aac2585a395718bacd588158171d9a04
SUDIP KUMAR SAH <bsse0934@iit.du.ac.bd>
2025-09-26 08:21:40 +0900

    work on jlpt scrape again

--- scraper-test/index.js
+++ scraper-test/index.js
@@ -324,6 +324,7 @@ async function scrapeAllTests(browser) {
         const urlStrings = [
 
           `${BASE_URL}${level}-${category}-exercise-${exerciseNum}/`,
+          `${BASE_URL}jlpt-${level}-${category}-exercise-${exerciseNum}/`,
           `${BASE_URL}jlpt-${level}-${category}-${exerciseNum}/`,
           `${BASE_URL}japanese-language-proficiency-test-jlpt-${level}-${category}-exercise-${String(exerciseNum).padStart(2, '0')}/`,
           `${BASE_URL}japanese-language-proficiency-test-jlpt-${level}-${category}-exercise-${exerciseNum}/`
